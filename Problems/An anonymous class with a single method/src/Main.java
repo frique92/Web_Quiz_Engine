c604de950bbc4d602e60839b9be8f59084c456bb
frique92 <29735207+frique92@users.noreply.github.com>
2020-05-08 17:35:43 +0300

    solve "Anonymous classes"

--- Problems/An anonymous class with a single method/src/Main.java
+++ Problems/An anonymous class with a single method/src/Main.java
@@ -1,2 +1,7 @@
-SingleMethodInterface instance = /* create an instance of an anonymous class here, 
-                                    do not forget ; in the end */
+SingleMethodInterface instance = new SingleMethodInterface() {
+
+    @Override
+    public void doSomething() {
+        System.out.println("The anonymous class does something");
+    }
+};
---
+++ Problems/Returning from a method/src/Main.java
@@ -0,0 +1,11 @@
+public static Runnable createRunnable(String text, int repeats) {
+    return new Runnable() {
+
+        @Override
+        public void run() {
+            for(int i = 0; i < repeats; i++) {
+                System.out.println(text);
+        }
+        }
+        };
+}
